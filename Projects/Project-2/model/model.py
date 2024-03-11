6c8d9ffbb9d958e76aa6a559cfb03d498743ce00
Yugal Khanter <yugalkhanter82@gmail.com>
2024-03-11 16:50:41 +0530

    completed the model training

--- Projects/Project-2/model/model.py
+++ Projects/Project-2/model/model.py
@@ -72,6 +72,7 @@ def decision_classifier(train_X, train_y, test_X, test_y):
     print("Classification report: \n", classification_report(test_y, y_pred))
     print("Confusion Matrix: \n", confusion_matrix(test_y, y_pred))
     print("Score: \n", classifier.score(test_X, test_y))
+    return y_pred
 
 # decision_classifier(train_X, train_y, test_X, test_y)
 
@@ -84,6 +85,7 @@ def naive_bayes(train_X, train_y, test_X, test_y):
     print("Classification report: \n", classification_report(test_y, y_pred))
     print("Confusion Matrix: \n", confusion_matrix(test_y, y_pred))
     print("Score: \n", model.score(test_X, test_y))
+    return y_pred
 
 # naive_bayes(train_X, train_y, test_X, test_y)
 
@@ -96,5 +98,61 @@ def random_forest(train_X, train_y, test_X, test_y):
     print("Classification report: \n", classification_report(test_y, y_pred))
     print("Confusion Matrix: \n", confusion_matrix(test_y, y_pred))
     print("Accuracy score: \n", accuracy_score(test_y, y_pred))
+    return y_pred
 
-# random_forest(train_X, train_y, test_X, test_y)
+y_pred = random_forest(train_X, train_y, test_X, test_y)
+
+combined = pd.DataFrame(dict(actual=test_y, prediction=y_pred))
+
+grouped_matches = df.groupby("team")
+# group = grouped_matches.get_group("Manchester United")
+
+def rolling_avgs(group, cols, new_cols):
+    group = group.sort_values("date")
+    rolling_stats = group[cols].rolling(3, closed="left").mean()
+    group[new_cols] = rolling_stats
+    group = group.dropna(subset=new_cols)
+    return group
+
+cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt"]
+new_cols = [f"{c}_rolling" for c in cols]
+# rolling_avgs(group, cols, new_cols)
+
+matches_rolling = df.groupby("team").apply(lambda x: rolling_avgs(x, cols, new_cols))
+matches_rolling = matches_rolling.droplevel("team")
+matches_rolling.index = range(matches_rolling.shape[0])
+
+def make_predictions(df, predictors):
+    train = df[df["date"] < "2022-01-01"]
+    test = df[df["date"] > "2022-01-01"]
+    train_X = train[predictors]
+    train_y = train["target"]
+    test_X = test[predictors]
+    test_y = test["target"]
+    y_preds = random_forest(train_X, train_y, test_X, test_y)
+    combined = pd.DataFrame(dict(actual=test_y, prediction=y_preds))
+    return y_pred, combined
+
+new_predictors = predictors + new_cols 
+y_preds, combined = make_predictions(matches_rolling, new_predictors)
+
+combined = combined.merge(matches_rolling[["date", "team", "opponent", "result"]], left_index=True, right_index=True)
+
+class MissingDict(dict):
+    __missing__ = lambda self, key: key
+
+map_values = {
+    "Brighton and Hove Albion": "Brighton",
+    "Manchester United": "Manchester Utd",
+    "Newcastle United": "Newcastle Utd",
+    "Tottenham Hotspur": "Tottenham",
+    "West Ham United": "West Ham",
+    "Wolverhampton Wanderers": "Wolves"
+}
+
+# this is done because there will be 2 rows that will display 1 match. i.e if Manchester United plays Arsenal, there will be one record in Arsenal group and one will be there in Manchester United group. For ex: Manchester United vs Arsenal at Old Trafford is same as Arsenal vs Manchester United (in arsenal group.)
+# And there might be a possibility that the model predicts 2 different outcomes for the same match. To avoid that, we merge the same data.
+mapping = MissingDict(**map_values)
+combined["new_team"] = combined["team"].map(mapping)
+merged = combined.merge(combined, left_on=["date", "new_team"], right_on=["date", "opponent"])
+merged[(merged["prediction_x"] == 1) & (merged["prediction_y"] == 0)]["actual_x"].value_counts()
